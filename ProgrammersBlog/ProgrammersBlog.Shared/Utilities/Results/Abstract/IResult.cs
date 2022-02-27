9f5d97aa784141f47ebfb51f28ac8f1e620c6bdc
yunuscoder <57803311+yunuscoder@users.noreply.github.com>
2022-02-27 21:48:13 +0300

    Asp.Net Core 5 Blog Sitesi Backend
    Asp.Net Core 5.0 Fluent API  ile geliştirilmiş blog sitesi

---
+++ ProgrammersBlog/ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; } //ResultStatus.Success 
+        public string Message { get; }
+        public Exception Exception { get; }
+
+    }
+}
---
+++ ProgrammersBlog/ProgrammersBlog.Shared/Utilities/Results/ComplexType/ResultStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.ComplexType
+{
+    public enum ResultStatus
+    { 
+        Success=0,
+        Error=1,
+        Warning=2, //ResultStatus.Warning şeklinde kullanılacak
+        Info=3
+    }
+}
